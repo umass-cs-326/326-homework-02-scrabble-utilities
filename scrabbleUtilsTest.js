31bf94125ab3f64e7034a5dc2fd92d2ba911d87a
Tim Richards <richards@cs.umass.edu>
2022-02-04 17:01:15 -0500

    Add files via upload

---
+++ scrabbleUtilsTest.js
@@ -0,0 +1,7 @@
+'use strict'; // Don't touch me.
+
+const utils = require("./scrabbleUtils.js");
+
+console.assert(utils.canConstructWord({ 'c': 1, 'f': 2, 'r': 1, 'b': 1, 'a': 1, 'e': 1 }, "crab"), "This error message will print if the test fails");
+
+// More tests...
